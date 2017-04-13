3244226335820a497cff2035ad8b14ab9d82267f
ysadre <ysadre8@gmail.com>
2017-04-13 11:56:00 +0100

    first commit

---
+++ api.js
@@ -0,0 +1,10 @@
+var url = "https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=FinjGw4gTsULHG5PCJLxifZNTfq8UPKPKI0UhO6M";
+
+
+$.ajax({
+  url: url,
+  success: function(result){
+  $("#date").text(result.date);
+  $("#returnObject").text(JSON.stringify(result, null, 4));  
+}
+});
